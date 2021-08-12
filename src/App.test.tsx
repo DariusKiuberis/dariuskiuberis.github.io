11976b5c3fc10905d1e53f37f3733355e07dad15
Darius Kiuberis <dariuskiuberis@gmail.com>
2021-08-12 19:06:41 +0100

    updated README, fixed App.test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,3 +6,7 @@ import App from './App'
 //     const linkElement = screen.getByText(/text component/i)
 //     expect(linkElement).toBeInTheDocument()
 // })
+
+test('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4)
+})
